6231aafbe66c3516cab08d0a5ddbe0802a7b9bcd
Sachin Lamba <lamba92sachin@gmail.com>
2018-11-24 03:04:37 +0530

    mail from  sendgrid comment helps

--- app.js
+++ app.js
@@ -659,3 +659,28 @@ function callProducts (filtersObject) {
 //           );
 //   });
 // }
+
+
+
+// echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+// echo "sendgrid.env" >> .gitignore
+// source ./sendgrid.env
+
+// npm install --save @sendgrid/mail
+//  "@sendgrid/mail": "^6.3.1",
+
+// // using SendGrid's v3 Node.js Library
+// // https://github.com/sendgrid/sendgrid-nodejs
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com',
+//   from: 'test@example.com',
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg);
+
+
+
